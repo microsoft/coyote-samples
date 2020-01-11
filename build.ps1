120a9b59b003efd3c679feb6f72235d03ddf910a
Chris Lovett <clovett@microsoft.com>
2020-01-11 02:27:40 +0000

    Merged PR 1063: Add updated version of FailureDetector because this is a handy sample used in our documentation
    Add updated version of FailureDetector because this is a handy sample used in "using-coyote" documentation that also happens to show how to use monitors.

--- build.ps1
+++ build.ps1
@@ -11,5 +11,6 @@ Write-Comment -prefix "." -text "Building the Coyote samples" -color "yellow"
 Invoke-DotnetBuild -dotnet $dotnet -solution "$PSScriptRoot\CloudMessaging\CloudMessaging.sln" -config $configuration
 Invoke-DotnetBuild -dotnet $dotnet -solution "$PSScriptRoot\Mocking\Mocking.sln" -config $configuration
 Invoke-DotnetBuild -dotnet $dotnet -solution "$PSScriptRoot\Nondeterminism\Nondeterminism.sln" -config $configuration
+Invoke-DotnetBuild -dotnet $dotnet -solution "$PSScriptRoot\Monitors\Monitors.sln" -config $configuration
 
 Write-Comment -prefix "." -text "Successfully built the Coyote samples" -color "green"
