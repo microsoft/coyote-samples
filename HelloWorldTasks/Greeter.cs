6159bdec580c0a52054848ae1937a1e085925144
Dimitre Novatchev <v-dino@microsoft.com>
2020-01-13 23:23:09 +0000

    Merged PR 1011: The HelloWorldTasks sample
    This is the code for the **_Hello World_** sample with the **_AsyncTasks_ _Programming Model._**
    
    I adapted a very simple existing example to store asynchronously strings in a shared object. The expected result is that the shared store must contain the string "Hello World!". However, due to a bug in the code, in some executions of this program the result is a different string.
    
    The `Specification.Assert()` call makes sure that any bug of this kind will not slip unnoticed. Indeed, once in a while when running
    
      `dotnet HelloWorldTasks.dll` (for netcoreapp2.2)
    
    or
    
      `HelloWorldTasks.exe`  (for .Net 4.7 or 4.6)
    
    we get this:
    
        Microsoft.Coyote.Runtime.AssertionFailureException: Value is 'Good Morning' instead of 'Hello World!'.

---
+++ HelloWorldTasks/Greeter.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using Microsoft.Coyote.Specifications;
+using Microsoft.Coyote.Threading.Tasks;
+
+namespace Microsoft.Coyote.Samples.HelloWorld
+{
+    internal class Greeter
+    {
+        private const string HelloWorld = "Hello World!";
+        private const string GoodMorning = "Good Morning";
+
+        internal class SharedEntry<T>
+        {
+            public T Value = default;
+        }
+
+        public static async ControlledTask WriteWithDelayAsync<T>(SharedEntry<T> entry, T value)
+        {
+            await ControlledTask.Delay(100);
+            entry.Value = value;
+        }
+
+        public async ControlledTask RunAsync()
+        {
+            SharedEntry<string> entry = new SharedEntry<string>();
+
+            ControlledTask task1 = WriteWithDelayAsync(entry, GoodMorning);
+            ControlledTask task2 = WriteWithDelayAsync(entry, HelloWorld);
+            ControlledTask task3 = WriteWithDelayAsync(entry, HelloWorld);
+
+            await ControlledTask.WhenAll(task1, task2, task3);
+
+            Console.WriteLine(entry.Value);
+
+            Specification.Assert(entry.Value == HelloWorld, $"Value is '{entry.Value}' instead of '{HelloWorld}'.");
+        }
+    }
+}
---
+++ HelloWorldTasks/Program.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Microsoft.Coyote.Runtime;
+using Microsoft.Coyote.Samples.HelloWorld;
+using Microsoft.Coyote.Threading.Tasks;
+
+namespace Microsoft.Coyote.Samples.HelloWorldTasks
+{
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            await Execute();
+        }
+
+        [Microsoft.Coyote.TestingServices.Test]
+        public static async ControlledTask Execute()
+        {
+            var greeter = new Greeter();
+            await greeter.RunAsync();
+        }
+    }
+}
