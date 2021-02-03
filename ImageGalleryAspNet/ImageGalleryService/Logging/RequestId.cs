dd63966effee46cea98775a31791adc8b06936b1
Pantazis Deligiannis <pdeligia@microsoft.com>
2021-02-03 11:53:50 -0800

    reverted the ASP.NET sample (#19)

---
+++ ImageGalleryAspNet/ImageGalleryService/Logging/RequestId.cs
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading;
+
+namespace ImageGallery.Logging
+{
+    public static class RequestId
+    {
+        private static readonly AsyncLocal<string> AsyncLocalInstance = new AsyncLocal<string>();
+
+        internal static string Create(string id)
+        {
+            AsyncLocalInstance.Value = id;
+            return id;
+        }
+
+        public static string Get() => AsyncLocalInstance.Value;
+    }
+
+    public class ApplicationLogs
+    {
+    }
+}
