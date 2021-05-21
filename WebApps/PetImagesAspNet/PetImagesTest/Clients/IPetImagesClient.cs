060470c3f600b64845252bd156342dc557de930a
Pantazis Deligiannis <pdeligia@microsoft.com>
2021-05-21 15:32:36 -0700

    web app sample (#31)

---
+++ WebApps/PetImagesAspNet/PetImagesTest/Clients/IPetImagesClient.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using PetImages.Contracts;
+
+namespace PetImagesTest.Clients
+{
+    public interface IPetImagesClient
+    {
+        public Task<ServiceResponse<Account>> CreateAccountAsync(Account account);
+    }
+}
---
+++ WebApps/PetImagesAspNet/readme.md
@@ -0,0 +1,18 @@
+## PetImagesAspNet
+
+To build the sample run:
+```
+dotnet build
+```
+
+To rewrite the sample for Coyote testing do:
+```
+coyote rewrite rewrite.coyote.json
+```
+
+To run the tests with Coyote do:
+```
+dotnet test bin/coyote/PetImagesTests.dll
+```
+
+More details on this sample are coming soon.
