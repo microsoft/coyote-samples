b2e33d9c0ca5f9bf6fec7da3bc05f9ada0217eff
Immad <immad.naseer@gmail.com>
2021-05-23 11:46:48 -0700

    add all public PetImages API to IPetImagesClient (#33)

--- WebApps/PetImagesAspNet/PetImagesTest/Clients/IPetImagesClient.cs
+++ WebApps/PetImagesAspNet/PetImagesTest/Clients/IPetImagesClient.cs
@@ -9,5 +9,13 @@ namespace PetImagesTest.Clients
     public interface IPetImagesClient
     {
         public Task<ServiceResponse<Account>> CreateAccountAsync(Account account);
+
+        public Task<ServiceResponse<Image>> CreateImageAsync(string accountName, Image image);
+
+        public Task<ServiceResponse<Image>> CreateOrUpdateImageAsync(string accountName, Image image);
+
+        public Task<ServiceResponse<byte[]>> GetImageAsync(string accountName, string imageName);
+
+        public Task<ServiceResponse<byte[]>> GetImageThumbnailAsync(string accountName, string imageName);
     }
 }
