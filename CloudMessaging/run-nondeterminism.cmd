57fd6a41f5df8a3f443232743f00b074739df307
Pantazis Deligiannis <pdeligia@microsoft.com>
2021-10-07 15:14:47 -0700

    edit

--- CloudMessaging/run-nondeterminism.cmd
+++ CloudMessaging/run-nondeterminism.cmd
@@ -1,2 +1,2 @@
 cd %~dp0
-dotnet ..\packages\microsoft.coyote.test\1.4.1\lib\net5.0\coyote.dll test ../bin/net5.0/Raft.Nondeterminism.dll -i 1000 -ms 500 -graph-bug
+dotnet ..\..\Coyote\bin\net5.0\coyote.dll test ../bin/net5.0/Raft.Nondeterminism.dll -i 1000 -ms 500 -graph-bug
