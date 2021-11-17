ca43192eb4962c5278768aa3dd6c3bde66ab8a35
Pantazis Deligiannis <pdeligia@microsoft.com>
2021-11-17 14:40:46 -0800

    update dotnet sdk (#47)

--- Scripts/build.ps1
+++ Scripts/build.ps1
@@ -6,18 +6,6 @@ param(
 
 Import-Module $PSScriptRoot\..\Common\helpers.psm1
 
-$dotnet_path = FindDotNet($dotnet)
-$sdk_version = FindDotNetSdk($dotnet_path);
-
-if ($null -eq $sdk_version)
-{
-    Write-Comment -text "The global.json file is pointing to version: $sdk_version but no matching version was found" -color "yellow"
-    Write-Comment -text "Please install .NET SDK version $sdk_version from https://dotnet.microsoft.com/download/dotnet-core." -color "yellow"
-    exit 1
-}
-
-Write-Comment -text "Using .NET SDK version $sdk_version" -color yellow
-
 Write-Comment -prefix "." -text "Building the Coyote samples" -color "yellow"
 
 # Build the task-based samples.
